c42952c9cc968ddef6bc120c5a3d3749b41dc2af
fabrizziorivero <Manzana22#>
2021-03-22 20:49:41 -0600

    Refactor in progress, adding endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,28 +1,50 @@
 import express, { Request, Response } from 'express';
 import paviModel from './models/pavi'
 import { processVideo } from './middleware/middleware'
+import mongodb from 'mongodb'
+import multer from 'multer';
+const path = require('path');
+import { uuid } from 'uuidv4';
 
 const router = express.Router()
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, "/public/uploads"),
+    filename: function (req, file, cb) {
+      const fullName =
+        "file" + uuid().replace(/-/g, "") + path.extname(file.originalname);
+      cb(null, fullName);
+    },
+  });
+const videoFile = multer({ storage: storage})
 
 //Get's all the videos
-router.post('/videos', [] ,(req: Request, res: Response)=>{
+router.post('/videos/upload', videoFile.single('video') ,(req: Request, res: Response)=>{
     //Call open vino so i can get the json it gives you and then save it.
     /**
      * Here the path should send you a file and the you pass it to the algorithm
      * You'll need to see how to load the video un download it. 
      */
-    processVideo();
-    const saveVideo = new paviModel(req.body)
-    saveVideo.save()
-    .then((result)=> res.send(result))
-    .catch((error)=> console.log(error))
+    res.json({
+        success:true,
+        video:{
+            videos: ''
+        }
+    })
 
     // return newVideo.save().then(()=> console.log("saved")).catch((error)=>console.log(error))
 })
 
 //Process uploaded video 
-router.get('/process_video/id', (req: Request, res: Response)=>{
-    return res.send('Here you make something to process the video')
+router.get('/videos', (_, res: Response)=>{
+    paviModel.find((err: any, pavi:any)=>{
+        return err ? res.send(err) : res.send(pavi)
+    })
+})
+
+router.get('/videos/:id/', (req: Request, res: Response)=>{
+    paviModel.findById(req.params.id, (err: any, pavi:any)=>{
+        return err ? res.send(err) : res.send(pavi)
+    })
 })
 
 export { router as videosRouter }
